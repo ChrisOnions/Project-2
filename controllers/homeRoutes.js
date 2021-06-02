3a45eb0deada2730f70d7b5d95bcfab97e6d8e73
ChrisOnions <c.onions101@gmail.com>
2021-06-02 21:55:39 +1000

    Added handlebars layouts files. Added styling for new style. Added images for html added base homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,13 +2,57 @@ const router = require('express').Router();
 // const withAuth = require('../utils/auth');
 
 
+router.get('/', async (req, res) => {
+  res.render('home')
+})
+
 router.get('/login', async (req, res) => {
 
   // if (req.session.logged_in) {
-  //   res.render('login')
+  //   res.render('home')
   //   return
-  // } else { res.redirect('/home') }
+  // } else { res.redirect('/login') }
   res.render('login')
 })
+router.get('/signup', async (req, res) => {
+
+  // if (req.session.logged_in) {
+  //   res.render('signup')
+  //   return
+  // } else { res.redirect('/login') }
+  res.render('signup')
+})
+
+router.get('/dashboard', async (req, res) => {
+
+  // if (req.session.logged_in) {
+  //   res.render('dashboard')
+  //   return
+  // } else { res.redirect('/login') }
+  res.render('dashboard')
+})
+router.get('/dashboard', async (req, res) => {
+
+  // if (req.session.logged_in) {
+  //   res.render('cart')
+  //   return
+  // } else { res.redirect('/login') }
+  res.render('dashboard')
+})
+
+
+router.get('/cart', async (req, res) => {
+
+  // if (req.session.logged_in) {
+  //   res.render('cart')
+  //   return
+  // } else { res.redirect('/login') }
+  res.render('cart')
+})
+//Last route
+router.get('*', async (req, res) => {
+  res.render('404')
+})
+
 
 module.exports = router;
