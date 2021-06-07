4048d06df6c84967b5cb304c0a0712bc3a3a045f
kunalc156 <kunal.choudhary156@gmail.com>
2021-06-07 19:38:30 +1000

    simplified the code

--- controllers/api/foodItemRoutes.js
+++ controllers/api/foodItemRoutes.js
@@ -3,34 +3,34 @@ const { FoodItem } = require('../../models');
 
 router.post('/create', async (req, res) => {
   try {
+    const { name, expiry_date,quantity,brand, food_category_id, donated, is_frozen,already_purchased} = req.body;
     const foodData = await FoodItem.findOne({ where: { name: req.body.name } });
 
     if (!foodData) {
         //create the food 
         const foodItem = await FoodItem.create({
-          name: req.body.name,
-          expiry_date: req.body.expiry_date,
-          quantity: req.body.quantity,
-          brand: req.body.brand,
-          food_category:id: req.body.food_category_id,
-          donated: req.body.donated,
-          is_frozen: req.body.is_frozen,
-          already_purchased: req.body.already_purchased
-
+         name,
+         expiry_date,
+         quantity,
+         brand,
+         food_category_id,
+         donated,
+         is_frozen,
+         already_purchased
         });
         res.status(200).json(foodItem);
     } else {
       //update the food
      const foodItem =  FoodItem.update(
         {
-          name: req.body.name,
-          expiry_date: req.body.expiry_date,
-          quantity: req.body.quantity,
-          brand: req.body.brand,
-          food_category:id: req.body.food_category_id,
-          donated: req.body.donated,
-          is_frozen: req.body.is_frozen,
-          already_purchased: req.body.already_purchased
+          name,
+          expiry_date,
+          quantity,
+          brand,
+          food_category_id,
+          donated,
+          is_frozen,
+          already_purchased
         },
         {
           //gets the food based on ID
@@ -48,20 +48,23 @@ router.post('/create', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
+  const { id } =  req.params;
+  const { name, expiry_date,quantity,brand, food_category_id, donated, is_frozen,already_purchased} = req.body;
+
   FoodItem.update(
     {
-      name: req.body.name,
-      expiry_date: req.body.expiry_date,
-      quantity: req.body.quantity,
-      brand: req.body.brand,
-      food_category:id: req.body.food_category_id,
-      donated: req.body.donated,
-      is_frozen: req.body.is_frozen,
-      already_purchased: req.body.already_purchased
+      name,
+      expiry_date,
+      quantity,
+      brand,
+      food_category_id,
+      donated,
+      is_frozen,
+      already_purchased
     },
     {
       where: {
-        id: req.params.id,
+        id
       },
     }
   )
@@ -75,9 +78,11 @@ router.put('/:id', async (req, res) => {
 // Delete route for a food item with a matching id
 router.delete('/:id', async (req, res) => {
   // Looks for the food item based on id given in the request parameters and deletes the instance from the database
+  const { id } =  req.params;
+
   FoodItem.destroy({
     where: {
-      id: req.params.id,
+      id
     },
   })
     .then((deletedItem) => {
@@ -96,10 +101,11 @@ router.get('/', async(req, res) => {
 
 // GET a food item
 router.get('/:id', async(req, res) => {
+  const { id } =  req.params;
   FoodItem.findOne(
     {
       where: { 
-        id: req.params.id 
+        id
       },
     }
   ).then((foodData) => {
