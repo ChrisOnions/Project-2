a5ecfea4471610750379ac0a3780d0369df7a866
kunalc156 <kunal.choudhary156@gmail.com>
2021-06-07 19:28:00 +1000

    updated donated user id and food item id

--- seeds/seedFoodBank.js
+++ seeds/seedFoodBank.js
@@ -6,16 +6,16 @@ const foodBankData = [
       address: "Shop 128 , 7 Albion road, boxhill, Vic, 3128",
       quantity: 12,
       donated_date: 8 / 04 / 2021,
-      donatedByUserId: null,
-      foodItemId: null,
+      donatedByUserId: 1,
+      foodItemId: 1,
     },
     {
         name: "United children relief fund",
         address: "Shop 128 , 88 camberwell road, camberwell, Vic, 3122",
         quantity: 11,
         donated_date: 5 / 02 / 2020,
-        donatedByUserId: null,
-        foodItemId: null,
+        donatedByUserId: 2,
+        foodItemId: 2,
     }
   ];
   
