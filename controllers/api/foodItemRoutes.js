ada118d734dad837ca3e5448deed90f15af6826c
kunalc156 <kunal.choudhary156@gmail.com>
2021-06-08 19:14:27 +1000

    updated food item api

--- controllers/api/foodItemRoutes.js
+++ controllers/api/foodItemRoutes.js
@@ -18,10 +18,9 @@ router.post('/create', async (req, res) => {
          is_frozen,
          already_purchased
         });
-        res.status(200).json(foodItem);
     } else {
       //update the food
-     const foodItem =  FoodItem.update(
+     const foodItem =  await FoodItem.update(
         {
           name,
           expiry_date,
@@ -38,79 +37,79 @@ router.post('/create', async (req, res) => {
             id: foodData.id,
           },
         });
-        res.status(200).json(foodItem);
   
     }
+    res.status(200).json(foodItem);
 
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
 router.put('/:id', async (req, res) => {
   const { id } =  req.params;
   const { name, expiry_date,quantity,brand, food_category_id, donated, is_frozen,already_purchased} = req.body;
-
-  FoodItem.update(
-    {
-      name,
-      expiry_date,
-      quantity,
-      brand,
-      food_category_id,
-      donated,
-      is_frozen,
-      already_purchased
-    },
-    {
-      where: {
-        id
+  try{
+   const updatedFood =  await FoodItem.update(
+      {
+        name,
+        expiry_date,
+        quantity,
+        brand,
+        food_category_id,
+        donated,
+        is_frozen,
+        already_purchased
       },
-    }
-  )
-    .then((updatedFood) => {
-      // Sends the updated food item as a json response
-      res.json(updatedFood);
-    })
-    .catch((err) => res.json(err));
+      {
+        where: {
+          id
+        },
+      });
+    res.json(updatedFood);
+  } catch(err) {
+    res.json(err);
+  }
+  
 });
 
 // Delete route for a food item with a matching id
 router.delete('/:id', async (req, res) => {
   // Looks for the food item based on id given in the request parameters and deletes the instance from the database
   const { id } =  req.params;
-
-  FoodItem.destroy({
-    where: {
-      id
-    },
-  })
-    .then((deletedItem) => {
-      res.json(deletedItem);
+  try{
+    const deletedItem = await FoodItem.destroy({
+      where: {
+        id
+      },
     })
-    .catch((err) => res.json(err));
+    res.json(deletedItem)
+  }catch( err){
+    res.json(err)
+  }
+
 });
 
 
 // GET all items
 router.get('/', async(req, res) => {
-  FoodItem.findAll().then((foodData) => {
-    res.json(foodData);
-  });
+  const foodData = await FoodItem.findAll().get({ plain: true });
+  res.json(foodData)
 });
 
 // GET a food item
 router.get('/:id', async(req, res) => {
   const { id } =  req.params;
-  FoodItem.findOne(
+  
+  const foodData = await FoodItem.findOne(
     {
       where: { 
         id
       },
     }
-  ).then((foodData) => {
-    res.json(foodData);
-  });
+  ).get({plain: true})
+  res.json(foodData)
+
 });
 
 
