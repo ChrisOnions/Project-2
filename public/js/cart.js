b416ed6dad8cee7e3222bfee26ba20b4d75368b4
ChrisOnions <c.onions101@gmail.com>
2021-06-11 22:52:27 +1000

    Updated Cart handlebars and Cart css Added js psudo code for cart

---
+++ public/js/cart.js
@@ -0,0 +1,33 @@
+const e = require("express")
+
+const reAddItem_btn = document.getElementById('reAddItem')
+const addCategory_btn = document.getElementById('addCategory')
+
+
+// Add Fooditem from existing 
+const addItem_func = async (event) => {
+  event.preventDefault();
+  // popup modal for items
+  // existing items
+  // display existing items
+  // get item details
+  // update item details
+  // expiryDate
+  // isFrozen
+  // session.user_id
+  // quantity
+  // brand
+  // Post request to server
+}
+// Add Category
+const addCategory_func = async (event) => {
+  event.preventDefault();
+  // Create modal for item to add
+  // 'name'
+  // 'is_perishable' checkbox
+  // if (name && bool)
+  // Fetch Post request to server
+
+}
+reAddItem_btn.addEventListener('click', addItem_func)
+addCategory_btn.addEventListener('click', addCategory_func)
